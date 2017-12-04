63e77b9c6b12eddbd39b015441b5d5b8dcceeeab
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-03 21:16:10 -0800

    adding find method and tests

--- lab-pedja/lib/linked-list.js
+++ lab-pedja/lib/linked-list.js
@@ -8,7 +8,39 @@ class LinkedList {
   }
 
   // TODO adding append, find and remove methods
-  
+  append(node){
+    if(!(node instanceof LinkedList))
+      throw new TypeError('<node> should be an instance of LinkedList');
+    if(!this.next)
+      this.next = node;
+    else
+      this.next.append(node);
+
+    return this;
+  }
+
+  find(node){
+    if(node === this.value)
+      return this;
+    if(this.next === null)
+      return null;
+    else
+      return this.next.find(node);
+  }
+
+  remove(node){
+    if(!(node instanceof LinkedList))
+      throw new TypeError('<node> should be instance of LinkedList');
+    if(!this.next)
+      return this;
+    if(this.next === node)
+      this.next = this.next.next;
+    else
+      this.next.remove(node);
+
+    return this;
+  }
+
 }
 
 
